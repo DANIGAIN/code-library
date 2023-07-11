22e961d7ae831ce82a81c255237f09c38756dd68
DANIGAIN <danigain1234@gmail.com>
2023-07-11 07:32:15 +0600

    update pascal triangle

--- Array/Pascal_Triangle.cpp
+++ Array/Pascal_Triangle.cpp
@@ -46,6 +46,21 @@ Solution 1 :
 
     nCr = n!/ r! * (n-r)!  
 
+    time : O(r)
+
+
+solution 2 : 
+
+n = 5 
+  
+   1 4/1 4*3/1*2   4*3*2/1*2*3  1 
+
+   forlula : (row - col) / col 
+
+   time : O(n)
+
+   space : O(1)
+
 
 */
 
@@ -63,15 +78,86 @@ long long funnCr(int n,int r)
     return ans ; 
 }
 
+void printNth(int n )
+{
+    int ans = 1 ;
+
+    cout<<ans <<" ";
+
+    for(int i = 1 ;i< n ;i++)
+    {
+        ans *= (n - i);
+        ans = ans /i ;
+        printf("%d ", ans);
+    }
+
+
+
+}
+
+
+std::vector<int>  printRow(int r)
+{
+    vector<int> row ;
+
+    row.push_back(1) ;
+    int ans = 1 ; 
+    for(int i = 1 ;i<r ;i++)
+    { 
+
+        ans *= (r -  i );
+        ans = ans /i ;
+        row.push_back(ans);
+
+    }
+    return row ;
+}
+
 int32_t main()
 {
 
 	//-------------------------------------- solution 1 ---------------------------//
 
 
-	long long ans = funnCr(4,2);
+	// long long ans = funnCr(4,2);
 
-	cout<<ans <<endl ;
+	// cout<<ans <<endl ;
+
+    //---------------------------------- problem 2 print any nth row ------------- //
+
+    // printNth(5) ;
+
+
+    //-------------------------------  solution 3 print spiral matrix --------------//
+
+
+    int n = 6 ;
+    vector<std::vector<int> > ans ;
+
+
+    for(int i =1 ;i <= n ;i++) 
+    {
+        ans.push_back(printRow(i));
+    }
+
+
+    for(int i = 0 ;i< ans.size() ;i++)
+    {
+        for(int j = 0 ;j< ans[i].size() ; j++)
+        {
+            cout<<ans[i][j]<<" ";
+        }
+
+        cout<<endl ; 
+    }
+
+
+
+
+
+
+
+
 
 
    return 0 ;
