f60d6dd77d00b62582d4be08cc930d4ba5d2fa82
DANIGAIN <danigain1234@gmail.com>
2025-02-21 09:55:50 +0600

    update pattern

--- basics/pattern.cpp
+++ basics/pattern.cpp
@@ -2,14 +2,15 @@
 using namespace std;
 int main(){
 	int n ;cin>>n;
-
+    cout<<"p1:"<<endl;
 	for(int i=0; i< n; i++){
 		for(int j=0;j<n;j++){
 			cout<<"*";
 		}
 		cout<<endl;
 	}
-
+    
+	cout<<"\np2:"<<endl;
 	for(int i=0; i< n; i++){
 		for(int j=0;j<=i;j++){
 			cout<<"*";
@@ -17,9 +18,34 @@ int main(){
 		cout<<endl;
 	}
 	
+	cout<<"\np3:"<<endl;
     for(int i =0; i<n; i++){
 	   for(int j=0; j<=i; j++){
-         cout<<j+1;
+           cout<<j+1;
+	   }
+	   cout<<endl;
+	}
+
+	cout<<"\np4:"<<endl;
+    for(int i =0; i<n; i++){
+	   for(int j=0; j<=i; j++){
+           cout<<i+1;
+	   }
+	   cout<<endl;
+	}
+
+	cout<<"\np5:"<<endl;
+    for(int i =0; i<n; i++){
+	   for(int j=0; j<n-i; j++){
+           cout<<"*";
+	   }
+	   cout<<endl;
+	}
+
+	cout<<"\np6:"<<endl;
+    for(int i =0; i<n; i++){
+	   for(int j=0; j<n-i; j++){
+           cout<<j+1;
 	   }
 	   cout<<endl;
 	}
