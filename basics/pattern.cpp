7c6cfcd74cfaf4f2a506e317525ee8bad89d3d2d
DANIGAIN <danigain1234@gmail.com>
2025-02-21 09:36:53 +0600

    add pattern

--- basics/pattern.cpp
+++ basics/pattern.cpp
@@ -11,10 +11,17 @@ int main(){
 	}
 
 	for(int i=0; i< n; i++){
-		for(int j=0;j<i+1;j++){
+		for(int j=0;j<=i;j++){
 			cout<<"*";
 		}
 		cout<<endl;
+	}
+	
+    for(int i =0; i<n; i++){
+	   for(int j=0; j<=i; j++){
+         cout<<j+1;
+	   }
+	   cout<<endl;
 	}
 	return 0;
 }
