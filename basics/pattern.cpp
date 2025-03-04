95e6e2097fcd07dd1435f33716b511e01e5e90a3
DANIGAIN <danigain1234@gmail.com>
2025-03-04 22:25:16 +0600

    update pattern list

--- basics/pattern.cpp
+++ basics/pattern.cpp
@@ -121,12 +121,103 @@ void pattern9(int n){
 		cout << endl;
 	}
 }
+void pattern10(int n){
+	cout << "\np10:" << endl;
+	for (int i = 0; i < n; i++){
+		for (int j = 0; j < i+1 ; j++){
+			cout << "*";
+		}
+		cout<<endl;
+	}
+	for (int i = 1; i < n; i++){
+		for (int j = 0; j < n-i ; j++){
+			cout <<"*";
+		}
+		cout<<endl;
+	}
+}
+void pattern11(int n){
+	cout << "\np10:" << endl;
+	for (int i = 1; i <= n; i++){
+		for (int j = 1; j <= i; j++){
+			cout << ((j+i)%2 == 0);
+		}
+		cout<<endl;
+	}
+}
+void pattern12(int n){
+	for (int i = 1; i <= n; i++){
+		for (int j = 1; j <= i; j++){
+			cout <<j;
+		}
+		for (int j = 1; j <= (n-i)*2;j++){
+			cout <<" ";
+		}
+		for (int j = i; j >= 1;j--){
+			cout <<j;
+		}
+		cout<<endl;
+	}
+}
+void pattern13(int n){
+	int k = 1;
+	for (int i = 1; i <= n; i++){
+		for (int j = 1; j <= i; j++){
+			cout <<k++<<" ";
+		}
+		cout<<endl;
+	}
+}
+void pattern14(int n){
+	for(int i = 1; i<= n; i++){
+		for(int j =1;j<=i;j++){
+			cout<<(char)(64+j);
+		}
+		cout<<endl;
+	}
+}
+void pattern15(int n){
+	for(int i = n; i>= 1; i--){
+		for(int j =1;j<=i;j++){
+			cout<<(char)(64+j);
+		}
+		cout<<endl;
+	}
+}
+void pattern16(int n){
+	for(int i = 1;i<= n; i++){
+		for(int j =1;j<=i;j++){
+			cout<<(char)(64+i);
+		}
+		cout<<endl;
+	}
+}
+void pattern17(int n){
+	for(int i = 1;i<= n; i++){
+		for(int j =1;j<=n-i;j++){
+			cout<<" ";
+		}
+		for(int j =1;j<=i;j++){
+			cout<<(char)(j+64);
+		}
+		for(int j =i-1 ;j>=1;j--){
+			cout<<(char)(j+64);
+		}
+		cout<<endl;
+	}
+}
+void pattern18(int n){
+	for(int i = 1;i<= n; i++){
+		for(int j =1;j<=i;j++){
+			cout<<(char)(64 +j + n -i);
+		}
+		cout<<endl;
+	}
+}
 int main()
 {
 	int n;
 	cin >> n;
-	pattern7(n);
-	pattern8(n);
-	pattern9(n);
+	pattern18(n);
 	return 0;
 }
