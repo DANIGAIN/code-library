4bc8c30f5dbeca35c3b1dd7ff1ed4fb0200624ea
DANIGAIN <danigain1234@gmail.com>
2025-03-07 21:30:46 +0600

    done pattern basic

--- basics/pattern.cpp
+++ basics/pattern.cpp
@@ -279,10 +279,22 @@ void pattern21(int n){
 	cout<<endl;
    }
 }
+void pattern22(int n){
+	for(int i=0; i<n*2-1; i++){
+	   for(int j=0; j<n*2-1;j++){
+		   int top =  i;
+		   int left = j;
+		   int right = (n*2-2) - j;
+		   int bottom = (n*2-2)-i;
+		   cout<<n-min(min(top ,left), min(right, bottom));
+	   }
+	   cout<<endl;
+	}
+}
 int main()
 {
 	int n;
 	cin >> n;
-	pattern21(n);
+	pattern22(n);
 	return 0;
 }
